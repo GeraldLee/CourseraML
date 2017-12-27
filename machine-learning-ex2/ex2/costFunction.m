1aabd6a95e404dabd3b9c564b05fe2a84091a161
Changgen Li <changli@microsoft.com>
2017-12-27 15:33:33 +0800

    Week 3 exercise Complete

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -3,7 +3,6 @@ function [J, grad] = costFunction(theta, X, y)
 %   J = COSTFUNCTION(theta, X, y) computes the cost of using theta as the
 %   parameter for logistic regression and the gradient of the cost
 %   w.r.t. to the parameters.
-
 % Initialize some useful values
 m = length(y); % number of training examples
 
@@ -20,11 +19,8 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
-
-
-
-
-
+J= -1 * sum( y .* log( sigmoid(X*theta) ) + (1 - y ) .* log( (1 - sigmoid(X*theta)) ) ) / m ; 
+grad = X' * (sigmoid(X*theta)-y)/m;
 
 
 % =============================================================
--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,10 +17,12 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
-
-
-
-
+h = sigmoid(X*theta);
+J= -1 * sum( y .* log( h ) + (1 - y ) .* ...
+log( (1 - h) ) ) / m ...
++lambda *(theta(2:end)'*theta(2:end))/(2*m); 
+grad(2:end) = X(:,2:end)'*(h-y)/m + (lambda/m)*theta(2:end);
+grad(1) = ((h-y)'*X(:,1))'/m ;
 
 % =============================================================
 
