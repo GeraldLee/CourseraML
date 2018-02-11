eb5e10a2362fc975e4526d4a80a9b7a484f4ba4a
Changgen Li <changli@microsoft.com>
2018-02-11 17:51:19 +0800

    finish ex6

--- machine-learning-ex6/ex6/emailFeatures.m
+++ machine-learning-ex6/ex6/emailFeatures.m
@@ -48,16 +48,12 @@ x = zeros(n, 1);
 %
 %
 
-
-for i=1:length(word_indices)
-
-    row = word_indices(i);
-
-    x(row) = 1;
-
-end
-
-
+k = length(word_indices);  
+for i = 1:k,  
+    if(x(word_indices(i)) == 0)  
+        x(word_indices(i)) = x(word_indices(i)) + 1;  
+    end  
+end  
 
 
 
